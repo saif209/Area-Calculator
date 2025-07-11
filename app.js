32eefec08f50f9338f90ca8d9fa7b915f234e7a2
Saif Ahamed <amd678105@gmail.com>
2025-07-11 08:59:38 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const bodyPaser = require("body-parser")
 const app = express()
 
-app.use(bodyPaser.urlencoded({ extended: true })); // ✅ good practice
+app.use(bodyPaser.urlencoded({ extended: true })); /
 app.use(bodyPaser.json());
 
 app.get("/", (req, res) => {
